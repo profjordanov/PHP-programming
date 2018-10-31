36a3993960b8a49052f9de7b42f9ce062a76e665
Jordan Jordanov <d_yugioh@abv.bg>
2018-10-31 09:33:11 +0200

    bussiness logic added

---
+++ MVC Fundamentals/Introduction to Object Oriented Programming/Computer Exercise/Business/MobilePhone.php
@@ -0,0 +1,15 @@
+<?php
+namespace Computer\Business; 
+class MobilePhone extends Mobile implements iTouchScreen {
+    private $leftClick = false;
+    public function moveFinger($currentX, $currentY, $offsetX, $offsetY) {
+        $newSetX = $currentX + $offsetX;
+        $newSetY = $currentY + $offsetY;
+        return [$newSetX, $newSetY];
+    }
+    public function clickFinger($click) {
+        if ($click === true) {
+            $this->leftClick = true;
+        }
+    }
+}
